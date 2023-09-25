027a26483e38766ddea66c6dff221fb3f1b21a9d
Francois CARON <francois.caron@renault.com>
2023-09-25 22:40:21 +0200

    Fix texture reset

--- src/saturn/tex.c
+++ src/saturn/tex.c
@@ -7,7 +7,6 @@ static render_texture_t textures[TEXTURES_MAX];
 
 static uint8_t* tex = (uint8_t*)MAP_CS1(MEM_HUNK_BYTES); //2MB on RAM cartridge, after meme hunk
 static uint32_t tex_len = 0;
-static uint32_t max_mem = 0;
 
 static int16_t textures_len = 0;
 
@@ -15,25 +14,22 @@ void *tex_bump(uint32_t size) {
 	error_if(tex_len + size >= MEM_HUNK_BYTES, "Failed to allocate %d bytes (%d)", size, tex_len);
 	uint8_t *p = &tex[tex_len];
 	tex_len += size;
-  if (max_mem < tex_len) {
-    max_mem = tex_len;
-    printf("%d Texture Mem => %d kB\n",__LINE__, max_mem/1024);
-  }
 	return p;
 }
 
 void tex_reset(uint16_t len) {
-  uint8_t *p = &tex[len];
-	uint32_t offset = (uint32_t)p - (uint32_t)tex;
-  if (textures_len == len) return;
-	error_if(offset > tex_len || offset > MEM_HUNK_BYTES, "Invalid tex reset");
-	tex_len = offset;
-  if (max_mem < tex_len) {
-    max_mem = tex_len;
-    printf("%d Texture Mem => %d kB\n",__LINE__, tex_len/1024);
-  }
-  textures_len = len;
+  if (textures_len == len) {
+		return;
+	}
+	tex_len = 0;
+	if (len != 0) {
+		error_if((len > textures_len) || (len > MEM_HUNK_BYTES), "Invalid tex reset");
+		rgb1555_t *new_end = (rgb1555_t *)&textures[len].pixels[textures[len-1].size.x*textures[len].size.y];
+		tex_len = (uint16_t)((uint32_t)new_end - (uint32_t)&tex[0]);
+	}
+	textures_len = len;
 }
+
 uint16_t allocate_tex(uint32_t width, uint32_t height, rgb1555_t *buffer) {
     uint16_t texture_index = textures_len;
     textures[textures_len++] = (render_texture_t){{width, height}, buffer};
