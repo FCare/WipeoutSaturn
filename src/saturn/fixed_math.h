3ee94d1607b0734104e01303151fac72f9b21c94
Francois CARON <francois.caron@renault.com>
2023-10-04 23:33:38 +0200

    Fix math functions

--- src/saturn/fixed_math.h
+++ src/saturn/fixed_math.h
@@ -4,10 +4,10 @@
 #include <yaul.h>
 #include "fix16_mat44.h"
 
-#define PLATFORM_2PI      FIX16_2PI /* pi */
-#define PLATFORM_PI       FIX16_PI /* pi */
-#define PLATFORM_PI_2     FIX16_PI_2 /* pi/2 */
-#define PLATFORM_PI_4     FIX16_PI_4 /* pi/4 */
+#define PLATFORM_2PI      0x0006487F /* pi */
+#define PLATFORM_PI       0x0003243F /* pi */
+#define PLATFORM_PI_2     0x00019220 /* pi/2 */
+#define PLATFORM_PI_4     0x0000C90F /* pi/4 */
 
 extern fix16_t sqrt(fix16_t x);
 extern fix16_t sin(fix16_t x);
@@ -15,6 +15,7 @@ extern fix16_t cos(fix16_t x);
 extern fix16_t tan(fix16_t x);
 extern fix16_t acos(fix16_t x);
 extern fix16_t fmod(fix16_t x, fix16_t y);
+extern fix16_t atan(fix16_t x);
 extern fix16_t atan2(fix16_t x, fix16_t y);
 extern fix16_t fabsf(fix16_t x);
 extern fix16_t floor(fix16_t x);
--- src/saturn/math.c
+++ src/saturn/math.c
@@ -1,26 +1,56 @@
 #include "fixed_math.h"
 
+#include <limits.h>
 #include <gamemath/fix16.h>
+
+static const fix16_t THREE_PI_DIV_4 = 0x00025B2F;       /*!< Fix16 value of 3PI/4 */
 
 fix16_t sqrt(fix16_t x) {
   return (fix16_t)fix16_sqrt((fix16_t)x);
 }
 
-fix16_t sin(fix16_t x) {
-  return fix16_sin(x);
+fix16_t sin(fix16_t inAngle) {
+  fix16_t tempAngle = inAngle % (PLATFORM_PI << 1);
+
+  if(tempAngle > PLATFORM_PI)
+		tempAngle -= (PLATFORM_PI << 1);
+	else if(tempAngle < -PLATFORM_PI)
+		tempAngle += (PLATFORM_PI << 1);
+
+	fix16_t tempAngleSq = fix16_mul(tempAngle, tempAngle);
+  fix16_t tempOut;
+	tempOut = fix16_mul(-13, tempAngleSq) + 546;
+	tempOut = fix16_mul(tempOut, tempAngleSq) - 10923;
+	tempOut = fix16_mul(tempOut, tempAngleSq) + 65536;
+	tempOut = fix16_mul(tempOut, tempAngle);
+
+  return tempOut;
 }
 
-fix16_t cos(fix16_t x) {
-  return fix16_cos(x);
+fix16_t cos(fix16_t inAngle) {
+  return fix16_sin(inAngle + (PLATFORM_PI >> 1));
 }
 
-fix16_t tan(fix16_t x) {
-  return fix16_tan(x);
+fix16_t tan(fix16_t inAngle) {
+  	return fix16_sdiv(fix16_sin(inAngle), fix16_cos(inAngle));
 }
 
-fix16_t acos(fix16_t x) {
-  //to be developed
-  return 0.0f;
+fix16_t asin(fix16_t x)
+{
+	if((x > FIX16_ONE)
+		|| (x < -FIX16_ONE))
+		return 0;
+
+	fix16_t out;
+	out = (FIX16_ONE - fix16_mul(x, x));
+	out = fix16_div(x, fix16_sqrt(out));
+	out = atan(out);
+	return out;
+}
+
+fix16_t acos(fix16_t x)
+{
+	return ((PLATFORM_PI >> 1) - asin(x));
 }
 
 fix16_t fmod(fix16_t x, fix16_t y) {
@@ -28,8 +58,37 @@ fix16_t fmod(fix16_t x, fix16_t y) {
   return x;
 }
 
-fix16_t atan2(fix16_t x, fix16_t y) {
-  return (fix16_t)fix16_atan2((angle_t)RAD2ANGLE(x), (angle_t)RAD2ANGLE(y));
+fix16_t atan2(fix16_t inY , fix16_t inX)
+{
+	fix16_t abs_inY, mask, angle, r, r_3;
+
+	/* Absolute inY */
+	mask = (inY >> (sizeof(fix16_t)*CHAR_BIT-1));
+	abs_inY = (inY + mask) ^ mask;
+
+	if (inX >= 0)
+	{
+		r = fix16_div( (inX - abs_inY), (inX + abs_inY));
+		r_3 = fix16_mul(fix16_mul(r, r),r);
+		angle = fix16_mul(0x00003240 , r_3) - fix16_mul(0x0000FB50,r) + PLATFORM_PI_4;
+	} else {
+		r = fix16_div( (inX + abs_inY), (abs_inY - inX));
+		r_3 = fix16_mul(fix16_mul(r, r),r);
+		angle = fix16_mul(0x00003240 , r_3)
+			- fix16_mul(0x0000FB50,r)
+			+ THREE_PI_DIV_4;
+	}
+	if (inY < 0)
+	{
+		angle = -angle;
+	}
+
+	return angle;
+}
+
+fix16_t atan(fix16_t x)
+{
+	return atan2(x, FIX16_ONE);
 }
 
 fix16_t fabsf(fix16_t x) {
