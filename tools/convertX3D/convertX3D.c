67a54913a0c23fc8c19fe11baf40f37f3c8c7264
LeCare <francois.caron.perso@gmail.com>
2024-01-03 12:01:23 +0100

    Wip parser

--- tools/convertX3D/convertX3D.c
+++ tools/convertX3D/convertX3D.c
@@ -11,14 +11,49 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <string.h>
 #include <libxml/parser.h>
+
+#define MESH_FLAG       0x1
+#define POLYGON_FLAG    0x2
+
+static xmlNode* getNodeNamed(xmlNode * root, const char * name) {
+  if (root->children == NULL) return NULL;
+  for (xmlNode *node = root->children; node; node = node->next) {
+    if ((node->type == XML_ELEMENT_NODE) && (strcmp(node->name, name)==0)) return node;
+    else {
+      xmlNode *temp = getNodeNamed(node, name);
+      if (temp != NULL) return temp;
+    }
+  }
+  return NULL;
+}
+
+
+static xmlNode* getShapeInGroup(xmlNode *root) {
+  if (root == NULL) return NULL;
+  for (xmlNode *node = root; node; node = node->next) {
+    if ((node->type == XML_ELEMENT_NODE) && (strcmp(node->name, "Shape")==0)) return node;
+  }
+  return NULL;
+}
+
+static void printChild(xmlNode* root) {
+  if (root->children == NULL) return;
+  for (xmlNode *node = root->children; node; node = node->next) {
+    printf("%s contains %s\n", root->name, node->name);
+  }
+}
+
+//Besoin de gerer le USE vs DEF. Pour le moment pas plus besoin que ca...
 
 int
 main(int argc, char **argv)
 {
     xmlDoc         *document;
-    xmlNode        *root, *first_child, *node, *scene;
+    xmlNode        *root, *group, *shape;
     char           *filename;
+    uint8_t              flag;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s filename.xml\n", argv[0]);
@@ -29,20 +64,140 @@ main(int argc, char **argv)
     document = xmlReadFile(filename, NULL, 0);
     root = xmlDocGetRootElement(document);
     // fprintf(stdout, "Root is <%s> (%i)\n", root->name, root->type);
-    first_child = root->children;
-    scene = NULL;
-    for (node = first_child; node; node = node->next) {
-        // fprintf(stdout, "\t Child is <%s> (%i)\n", node->name, node->type);
-        if ((node->type == XML_ELEMENT_NODE) && (strcmp(node->name, "Scene")==0))
-        {
-          scene = node;
-          break;
-        }
-    }
-    if (scene == NULL) {
-      fprintf(stdout, "No scene found\n");
+    group = getNodeNamed(root, "Group");
+    if (group == NULL) {
+      fprintf(stdout, "No group found\n");
+      xmlFreeDoc(document);
       return -1;
     }
-    // fprintf(stdout, "...\n");
+
+    for(shape = group->children; shape = getShapeInGroup(shape); shape = shape->next) {
+      xmlNode *material = getNodeNamed(shape, "Material");
+      if (material == NULL) {
+        fprintf(stdout, "No material found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+      printf("Material name = %s\n", xmlGetProp(material, "DEF"));
+      if (strcasestr(xmlGetProp(material, "DEF"), "colored") != NULL)  flag |= POLYGON_FLAG;
+      if (strcasestr(xmlGetProp(material, "DEF"), "fire") != NULL)     flag |= MESH_FLAG;
+      xmlNode *faceset = getNodeNamed(shape, "IndexedFaceSet");
+      if (faceset == NULL) {
+        fprintf(stdout, "No faceset found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+      if(strcmp(xmlGetProp(faceset, "normalPerVertex"), "true")!=0) {
+        printf("No normals - not supported\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+      if(strcmp(xmlGetProp(faceset, "colorPerVertex"), "false")!=0) {
+        printf("Colors per vertex - not supported\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      char* texIndex = xmlGetProp(faceset, "texCoordIndex");
+      int nbFaces = 0;
+      int nbVertexForFaces = 0;
+      char *str;
+      while((str=strsep(&texIndex, " ")) != NULL) {
+        if ((int) strtol(str, NULL, 10) == -1) nbFaces++;
+        else if (strlen(str)>0) nbVertexForFaces++;
+      }
+      printf("nbFaces = %d with nbVertex %d\n", nbFaces,nbVertexForFaces);
+
+      xmlNode *coordNode = getNodeNamed(shape, "Coordinate");
+      if (coordNode == NULL) {
+        fprintf(stdout, "No Coordinate found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      int nbVertex = 0;
+      char* DEFIndex = xmlGetProp(coordNode, "DEF");
+      if (DEFIndex != NULL) {
+        char* coordIndex = xmlGetProp(coordNode, "point");
+        while((str=strsep(&coordIndex, " ")) != NULL) {
+          if (strlen(str)>0) nbVertex++;
+        }
+        if ((nbVertex%3) != 0) {
+          printf("Error with vertex number %d\n", nbVertex);
+          xmlFreeDoc(document);
+          return -1;
+        }
+        printf("nbVertex = %d\n", nbVertex/3);
+      }
+
+      xmlNode *normNode = getNodeNamed(shape, "Normal");
+      if (normNode == NULL) {
+        fprintf(stdout, "No Normal found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      int nbNormals = 0;
+      if(xmlGetProp(normNode, "DEF") != NULL) {
+        char* coordIndex = xmlGetProp(normNode, "vector");
+        while((str=strsep(&coordIndex, " ")) != NULL) {
+          if (strlen(str)>0) nbNormals++;
+        }
+        if ((nbNormals%3) != 0) {
+          printf("Error with vertex number %d\n", nbNormals);
+          xmlFreeDoc(document);
+          return -1;
+        }
+        printf("nbNormals = %d\n", nbNormals/3);
+      }
+
+      if (nbNormals != nbVertex) {
+        printf("Not a normal per vertex\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      xmlNode *texcoordNode = getNodeNamed(shape, "TextureCoordinate");
+      if (texcoordNode == NULL) {
+        fprintf(stdout, "No Coordinate found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      int nbUv = 0;
+      char* texcoordIndex = xmlGetProp(texcoordNode, "point");
+      while((str=strsep(&texcoordIndex, " ")) != NULL) {
+        if (strlen(str)>0) nbUv++;
+      }
+      if ((nbUv%2) != 0) {
+        printf("Error with uv number %d\n", nbUv);
+        xmlFreeDoc(document);
+        return -1;
+      }
+      printf("nbUv = %d\n", nbUv/2);
+
+      xmlNode *colorNode = getNodeNamed(shape, "ColorRGBA");
+      if (colorNode == NULL) {
+        fprintf(stdout, "No Coordinate found\n");
+        xmlFreeDoc(document);
+        return -1;
+      }
+
+      int nbColor = 0;
+      char* colorIndex = xmlGetProp(colorNode, "color");
+      while((str=strsep(&colorIndex, " ")) != NULL) {
+        if (strlen(str)>0) nbColor++;
+      }
+      if ((nbColor%4) != 0) {
+        printf("Error with color number %d\n", nbColor);
+        xmlFreeDoc(document);
+        return -1;
+      }
+      printf("nbColor = %d\n", nbColor/4);
+
+
+    }
+
+    xmlFreeDoc(document);
     return 0;
 }
