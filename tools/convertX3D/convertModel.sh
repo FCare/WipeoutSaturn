7acd50309ba885dde44049a6df8b8ee410c4a8eb
Francois CARON <francois.caron@renault.com>
2024-01-13 14:24:12 +0100

    Add collission files

--- tools/convertX3D/convertModel.sh
+++ tools/convertX3D/convertModel.sh
@@ -9,4 +9,13 @@
 ./build/convertX3D ../blender/results/jacko.x3d ../blender/results/jacko\ end.png
 ./build/convertX3D ../blender/results/sophia.x3d ../blender/results/sophia_end.png
 
+./build/convertX3D ../blender/results/col_solaar.x3d
+./build/convertX3D ../blender/results/col_anasta.x3d
+./build/convertX3D ../blender/results/col_arial.x3d
+./build/convertX3D ../blender/results/col_arian.x3d
+./build/convertX3D ../blender/results/col_chang.x3d
+./build/convertX3D ../blender/results/col_dekka.x3d
+./build/convertX3D ../blender/results/col_jacko.x3d
+./build/convertX3D ../blender/results/col_sophia.x3d
+
 cp ../blender/results/*.smf ../../cd/wipeout/ship/
